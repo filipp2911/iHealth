02f9abf9141a87e5207c5f2c56ffd677e47ca752
Filipp Krupnov <philkrupnov@gmail.com>
2020-12-02 15:57:52 +0600

    Initial Commit

---
+++ iHealth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iHealth
+//
+//  Created by Filipp Krupnov on 2/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
